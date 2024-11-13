3e56b81b7eca0614ce6967bc6d8c953a58265646
Chrisw131 <walkerchris0523@gamil.com>
2024-11-13 15:00:56 -0500

    Lab303.6.1

---
+++ src/arraydemoTwo.java
@@ -0,0 +1,24 @@
+
+public class arraydemoTwo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		  // create an array
+		   int[] age = {12, 4, 5};
+
+		   // loop through the array
+		   // using for loop
+		   System.out.println("Using for Loop:");
+		   for(int i = 0; i < age.length; i++) {
+		     System.out.println(age[i]);
+		   }
+		 }
+		}
+
+// Output
+// Using for Loop:
+// 12
+// 4
+// 5
+// In the above example, we are using the for Loop in Java to iterate through each element of the array. Notice the expression inside the loop, age.length
+
